565ad48d868be5f59b3139753fc2d5c69cd0780f
Obidbektemirov <obidbektemirov112@gmail.com>
2025-10-30 06:47:41 +0000

    Добавлено отображение участников для внеклассных мероприятий и улучшен стиль списка участников

--- src/app.py
+++ src/app.py
@@ -85,7 +85,7 @@ def root():
 
 @app.get("/activities")
 def get_activities():
-    return activities
+    return {activity: {**details, "participants": details["participants"]} for activity, details in activities.items()}
 
 
 @app.post("/activities/{activity_name}/signup")
